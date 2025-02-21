a2feeb02353f8d5cd066d3df4d00000a2351e5b1
samperochkin <samuel.perreault@utoronto.ca>
2025-02-21 14:10:33 -0500

    updated canada script for guowen

--- applications/canada/for-guowen.R
+++ applications/canada/for-guowen.R
@@ -31,16 +31,33 @@ names(data)[grepl("o3", names(data))] <- "o3"
 names(data)
 
 data$spm25 <- sqrt(data$pm25)
+data$sno2 <- sqrt(data$no2)
+data$so3 <- sqrt(data$o3)
 data <- removeHolidays(data, "rm_all")
 
 
 # Fit models --------------------------------------------------------------
 knots_pm25 <- seq(floor(min(data$pm25)/5)*5,ceiling(max(data$pm25)/5)*5,5)
 knots_spm25 <- sqrt(knots_pm25)
+range(data$pm25)
+
+knots_no2 <- seq(floor(min(data$no2)/5)*5,ceiling(max(data$no2)/5)*5,5)
+knots_sno2 <- sqrt(knots_sno2)
+range(data$no2)
+
+knots_o3 <- seq(floor(min(data$o3)/5)*5,ceiling(max(data$o3)/5)*5,5)
+knots_so3 <- sqrt(knots_so3)
+range(data$o3)
+
 knots_temp <- seq(floor(min(data$temp)/5)*5,ceiling(max(data$temp)/5)*5,5)
 range(data$temp)
+
+
+
 formula <- count ~ f(date, model = "iid") +
   f(spm25, model = "hiwp", p=2, ref_value = sqrt(15), knots = knots_spm25, group_var = cdcode) +
+  # f(sno2, model = "hiwp", p=2, ref_value = 10, knots = knots_sno2, group_var = cdcode) +
+  # f(so3, model = "hiwp", p=2, ref_value = 20, knots = knots_so3, group_var = cdcode) +
   f(temp, model = "hiwp", p=2, ref_value = 10, knots = knots_temp, group_var = cdcode)
 
 cc_design <- ccDesign(time_var = "date", strat_vars = "cdcode")
@@ -55,9 +72,41 @@ max(fit$theta_info$map)
 split(fit$theta_info$var, fit$theta_info$map)
 
 # results per group
-ref_values <- list("spm25" = sqrt(15), "temp" = 10)
+ref_values <- list("spm25" = sqrt(15), "temp" = 10, "no2" = 10, "o3" = 20)
+knots <- list("spm25" = knots_spm25, "temp" = knots_temp, "no2" = knots_no2, "o3" = knots_o3)
+
 
 # HERE. WHAT TO DO WITH OVERDISPERSION TERMS!! THERE 
+all_vars <- sapply(fit$terms, "[[", "var") |> unique()
+ress <- lapply(c("temp", "spm25", "sno2", "so3"), \(x){
+  
+  if(!(x %in% all_vars)) return(NULL)
+  
+  res1 <- getEffect(fit,
+                    exposure_var = x, 
+                    group_var = "cdcode", group = unique(data$cdcode), 
+                    values = knots[[x]][1]:rev(knots[[x]])[1],
+                    ref_values = ref_values)
+  res2 <- getEffect(fit, 
+                    exposure_var = x, 
+                    group_var = "cdcode", group = NA, # for global effect
+                    values = knots[[x]][1]:rev(knots[[x]])[1],
+                    ref_values = ref_values)
+  
+  if(x != "temp"){
+    res1$var_value <- res1$var_value^2
+    res2$var_value <- res2_pm$var_value^2
+    res1$variable <- res2$variable <- substr(x, 2, 100)
+  }
+  list(res1 = res1, res2 = res2)
+})
+
+res1 <- lapply(ress, "[[", "res1") |> do.call(what = "rbind")
+res2 <- lapply(ress, "[[", "res2") |> do.call(what = "rbind")
+
+
+
+
 res1_temp <- getEffect(fit, exposure_var = "temp", 
                        group_var = "cdcode", group = unique(data$cdcode), 
                        values = knots_temp[1]:rev(knots_temp)[1],
