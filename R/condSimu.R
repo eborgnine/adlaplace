23e5d607694074002eee077d43dceae7eb610046
eborgnine <patrick.borgnine@gmail.com>
2025-05-12 15:38:14 -0400

    simu

--- R/condSimu.R
+++ R/condSimu.R
@@ -1,6 +1,7 @@
 #' @export
-condSim = function(fit,
+condSimIwp = function(fit,
                    term,
+                   x,
                    Nsim = 500,
                    Nvalues = 1001) {
   termsHere =   grep(term, unlist(lapply(fit$terms, function(xx)
@@ -8,14 +9,10 @@ condSim = function(fit,
   modelHere = unlist(lapply(fit$terms[termsHere], function(xx)
     xx$model))
   
-  if (all(modelHere == 'iid')) {
-    gammaHere = grep(term, fit$gamma_info$var)
-    Sx1 = colnames(fit$obj$env$data$A)[gammaHere]
-    Sx = gsub(paste0("(factor[(])?", term, "[)]?"), "", Sx1)
-    term2 = gsub(Sx[1], "", Sx1[1])
-    newDf = data.frame(newx = factor(Sx))
-    names(newDf) = term
-  } else {
+  if(!(all(modelHere == 'iwp'))) {
+    warning("model should be iwp to use condSimIwp")
+  }
+  
     ref_value = unique(unlist(lapply(fit$terms[termsHere], function(xx)
       xx$ref_value)))
     if (length(ref_value) > 1)
@@ -25,8 +22,7 @@ condSim = function(fit,
     Sx = seq(theRange[1], theRange[2], len = Nvalues)
     newDf = data.frame(newx = Sx)
     names(newDf) = term
-  }
-  
+
   newXA = hpoltest:::getNewXA(fit$terms, newDf)
   
   
@@ -63,3 +59,36 @@ condSim = function(fit,
   
   result = list(x = Sx, y = iwpSim, mean = parametricPart)
 }
+
+condSimHiwp = function(fit,
+                      term,
+                      Nsim = 500,
+                      Nvalues = 1001) {
+ 
+  simIwp = condSimIwp(fit, term, Nsim, Nvalues)
+   
+}
+  
+condSimIid = function(fit,
+                      term,
+                      Nsim = 500) {
+  
+  termsHere =   grep(term, unlist(lapply(fit$terms, function(xx)
+    xx$var)))
+  modelHere = unlist(lapply(fit$terms[termsHere], function(xx)
+    xx$model))
+  
+  if(!(all(modelHere == 'iid'))) {
+    warning("model should be iid to use condSimIid")
+  }
+    gammaHere = grep(term, fit$gamma_info$var)
+    Sx1 = colnames(fit$obj$env$data$A)[gammaHere]
+    Sx = gsub(paste0("(factor[(])?", term, "[)]?"), "", Sx1)
+    term2 = gsub(Sx[1], "", Sx1[1])
+    newDf = data.frame(newx = factor(Sx))
+    names(newDf) = term
+}
+
+# function not done yet
+}
+                      
