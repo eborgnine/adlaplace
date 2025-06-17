ac55dd07e2d86d806d47e670f053087cacb98a05
eborgnine <patrick.borgnine@gmail.com>
2025-06-16 20:18:04 -0400

    test

--- hpol/src2/omp.cpp
+++ hpol/src2/omp.cpp
@@ -1,6 +1,5 @@
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::plugins(openmp)]]
-// [[Rcpp::depends(RcppEigen)]]
 #include <Rcpp.h>
 #include <cppad/cppad.hpp>
 #include <omp.h>
@@ -10,7 +9,7 @@ bool in_parallel() { return omp_in_parallel() != 0; }
 size_t thread_number() { return static_cast<size_t>(omp_get_thread_num()); }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector parallel_jacobian(Rcpp::NumericVector x, int num_threads = 2) {
+Rcpp::List parallel_jac_hess(Rcpp::NumericVector x, int num_threads = 2) {
     typedef CppAD::AD<double> ADdouble;
     typedef CppAD::vector<ADdouble> ADvec;
     typedef CppAD::vector<double> dvec;
@@ -23,9 +22,9 @@ Rcpp::NumericVector parallel_jacobian(Rcpp::NumericVector x, int num_threads = 2
     for(size_t i = 0; i < nx; ++i)
         ax[i] = x[i];
     CppAD::Independent(ax);
-    ay[0] = ax[0];
-    for(size_t i = 1; i < nx; ++i)
-        ay[0] *= ax[i];
+    ay[0] = 0;
+    for(size_t i = 0; i < nx; ++i)
+        ay[0] += ax[i] * ax[i] * ax[i];
     ADFun fun(ax, ay);
 
     // 2. Threading setup
@@ -34,15 +33,16 @@ Rcpp::NumericVector parallel_jacobian(Rcpp::NumericVector x, int num_threads = 2
     CppAD::parallel_ad<double>();
     CppAD::thread_alloc::hold_memory(true);
 
-    // 3. Parallel Jacobian calculation
-    Rcpp::NumericVector jacobian(nx);
+    // 3. Value and Jacobian calculation
     dvec x0(nx);
     for(size_t i = 0; i < nx; ++i) x0[i] = x[i];
-
     std::vector<ADFun> fun_threads(num_threads);
     for(int i = 0; i < num_threads; ++i)
         fun_threads[i] = fun;
 
+    double value = fun.Forward(0, x0)[0];
+
+    Rcpp::NumericVector jacobian(nx);
     #pragma omp parallel for
     for(int j = 0; j < nx; ++j) {
         size_t tid = thread_number();
@@ -52,25 +52,43 @@ Rcpp::NumericVector parallel_jacobian(Rcpp::NumericVector x, int num_threads = 2
         jacobian[j] = dy[0];
     }
 
-    // 4. Cleanup
+    // 4. Hessian calculation (parallelize by columns for safety)
+    Rcpp::NumericMatrix hessian(nx, nx);
+    #pragma omp parallel for
+    for(int j = 0; j < nx; ++j) {
+        size_t tid = thread_number();
+        dvec w(1, 1.0);
+        dvec u(nx, 0.0);
+        u[j] = 1.0;
+        fun_threads[tid].Forward(0, x0);
+        fun_threads[tid].Forward(1, u);
+        dvec ddw = fun_threads[tid].Reverse(2, w);
+        for(size_t i = 0; i < nx; ++i)
+            hessian(i, j) = ddw[2 * i + 1];
+    }
+
+    // 5. Cleanup
     CppAD::thread_alloc::parallel_setup(1, nullptr, nullptr);
     CppAD::thread_alloc::hold_memory(false);
     for(int i = 1; i < num_threads; ++i)
         CppAD::thread_alloc::free_available(i);
     CppAD::thread_alloc::free_available(0);
 
-    return jacobian;
+    return Rcpp::List::create(
+        Rcpp::Named("value") = value,
+        Rcpp::Named("jacobian") = jacobian,
+        Rcpp::Named("hessian") = hessian
+    );
 }
+
 
 #ifdef UNDEF
 
-Sys.setenv("PKG_CPPFLAGS" = "-I/opt/homebrew/include")
+
 
 library(Rcpp)
 sourceCpp("hpol/src2/omp.cpp")
+parallel_jac_hess(c(2, 3, 4), num_threads = 2)
 
-x <- c(2, 3, 4)
-parallel_jacobian(x, num_threads = 4)
-# Should return c(3*4, 2*4, 2*3) = c(12, 8, 6)
 
 #endif
