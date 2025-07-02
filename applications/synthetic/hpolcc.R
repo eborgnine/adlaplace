65bf6a30c4895d1ceb24ab4f87045b0c0b9bb6da
eborgnine <patrick.borgnine@gmail.com>
2025-07-02 13:10:44 -0400

    sim

--- applications/synthetic/hpolcc.R
+++ applications/synthetic/hpolcc.R
@@ -1,8 +1,13 @@
 
-data = readRDS("simData.rds")
-betaHat  = c(1.0858353, 0.2385639)
-logPrecHat = c( 5.6024098, 4.0603066, 5.5502284, 6.9152824)
+xx = readRDS("simData.rds")
+data = xx$data 
+
+betaHat  = xx$par[grep("beta", names(xx$par))]
+logPrecHat = xx$par[grep("theta", names(xx$par))]
 sigmaHat = exp(logPrecHat)^(-1/2)
+
+gamma_start = xx$par[grep("gamma", names(xx$par))]
+
 
   knots_pm <- seq(0, max(ceiling(max(data$pm)/5)*5, 20), 2.5)
 
@@ -11,18 +16,46 @@ sigmaHat = exp(logPrecHat)^(-1/2)
             paste(knots_pm, collapse = ", "))
   ) 
 
+library(hpolcc)
 
   cc_design <- ccDesign(time_var = "date", strat_vars = "region")
 
-library(hpolcc)
 
 fit <- hnlm(formula, data, cc_design = cc_design, 
  dirichelet= FALSE, for_dev=TRUE)
 
 
-gamma_start = rep(0, nrow(fit$tmb_data$ATp))
 
-res = loglik(MLE, 
+fit$config$num_threads = 8
+fit$config$transform_theta = FALSE
+
+
+	# don't remaximize
+res1 = hpolcc:::objectiveFunctionC( 
+ c(betaHat, gamma_start, sigmaHat),
+ fit$tmb_data, 
+ fit$config
+  )
+res1$h2 = Matrix::forceSymmetric(res1$hessian)
+res1$hessian[1:5,1:5]
+xx$h[1:5,1:5]
+
+allPar = c(betaHat, gamma_start, sigmaHat)
+bob = function(x) {
+	par = c(x,  allPar[-(1:length(x))])
+	configHere = fit$config
+	configHere$maxDeriv = 0
+	hpolcc:::objectiveFunctionC( 
+ 		c(betaHat, gamma_start, sigmaHat),
+ 		fit$tmb_data, configHere
+  )$value
+}
+bob(allPar)
+
+res1$cholHessian = Matrix::chol(res1$h2)
+
+# remaximize
+res = loglik(c(betaHat, sigmaHat), 
              gamma_start, 
              data=fit$tmb_data, 
     config = fit$config,
@@ -32,7 +65,7 @@ res = loglik(MLE,
 
 
 cholVariance = Matrix::solve(res$cholHessian)
-quantile(sqrt(Matrix::diag(cholVariance)))
+
 
 Nsim = 200
 gammaSim =  res$gamma_hat + 
@@ -60,4 +93,6 @@ matplot(
   type='l', col='#00000020', lty=1, xaxs='i',
   yaxs='i'
 )
+
+
 
