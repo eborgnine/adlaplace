d184b5ff9c4fba42a4eaa074774a4b1674d7e1dd
eborgnine <patrick.borgnine@gmail.com>
2025-11-03 20:50:47 -0500

    stuff

--- hpol/R/make_trustoptim_wrappers.R
+++ hpol/R/make_trustoptim_wrappers.R
@@ -1,13 +1,13 @@
 # cache must have gamma_start, Nfun, Nge
 #' @export
 wrappers_outer = list( 
-  fn = function(x, data, config, adFun, adFunFull, control_inner, cache) {
+  fn = function(x, data, config, adFunFull, control_inner, cache) {
     assign("Nfun", get("Nfun", cache)+1, cache)
     assign("last.par", x, envir=cache)
     result=try(loglik(x,
       gamma_start = get("gamma_start", envir=cache), 
       data=data, config=config, 
-        adFun = adFun, adFunFull = adFunFull,
+        adFunFull = adFunFull,
       control=control_inner, 
       deriv=0))
       if("file" %in% ls(cache)) {
@@ -17,11 +17,11 @@ wrappers_outer = list(
       assign("gamma_start", result$solution, envir=cache)
       result$minusLogLik
     },
-  gr = function(x, data, config, adFun, adFunFull, control_inner, cache) {
+  gr = function(x, data, config, adFunFull, control_inner, cache) {
     assign("Ngr", get("Ngr", cache)+1, cache)
     result= try(loglik(x,
         gamma_start = get("gamma_start", envir=cache), 
-        adFun = adFun, adFunFull = adFunFull,
+        adFunFull = adFunFull,
         data=data, config=config, control=control_inner))
       if("file" %in% ls(cache)) {
         if('try-error' %in% class(result) ) {result = list(minusLogLik = NA, deriv = list(dL = NA))}
